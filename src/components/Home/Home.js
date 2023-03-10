71046d0f759a7f9a87c52b73eabaad8051fcbbff
Nishaj <njnishaj0@gmail.com>
2023-03-10 22:04:19 +0530

    compeleted home's eye design work

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,22 @@
-import './Home.css'
+import "./Home.css";
 export default function Home() {
-   return(
-      <div className='home'>
-         <div className='home--img-div home--col-1'>
-            <div className='home--img-gradient'>
-            <img className='home--img' alt='' src={require('../../assets/sharingan_3.webp')}/>
-            </div>
-         </div>
-         <div className='home--h-div home--col-2'>
-            <h2 className='home--h2'>Villains</h2>
-            <h2 className='home--h3'>are not born they made by society</h2>
-         </div>
+  return (
+    <div className="home">
+      <div className="home--img-div home--col-1">
+        <div className="home--img-eye-shade">
+          <div className="home--img-eye">
+            <img
+              className="home--img"
+              alt=""
+              src={require("../../assets/sharingan_3.webp")}
+            />
+          </div>
+        </div>
       </div>
-   )
+      <div className="home--h-div home--col-2">
+        <h2 className="home--h2">Villains</h2>
+        <h2 className="home--h3">are not born they made by society</h2>
+      </div>
+    </div>
+  );
 }
