ab37dabe7063dfaf4e9619bff82f48fce195f692
Nishaj <njnishaj0@gmail.com>
2023-04-12 18:11:02 +0530

    trying to fix bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Navbar } from "./components/";
+import Navbar  from "./components/navbar/Navbar";
 import { Home } from "./containers";
 
 import "./index.css";
